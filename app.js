077b65a441e5a2ddc7c8207b4e500fc32edc7852
Mike Vautour <mike@iglab.dev>
2023-06-19 11:35:15 -0400

    added version to console log

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const app = express()
 // Heroku dynamically sets a port
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000
-
+const VERSION = '1.0.1'
 app.get('/version', (_req, res) => {
-  res.send('1.0.1')
+  res.send(VERSION)
 })
+
 app.get('/health', (req, res) => {
   res.send('ok')
 })
@@ -18,4 +19,6 @@ app.use(express.static('dist'))
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5000')
+  // eslint-disable-next-line no-console
+  console.log('version: ', VERSION)
 })
