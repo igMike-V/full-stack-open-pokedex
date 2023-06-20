fe8a22d5246cd5a71d76d596bc20983c8d384829
Mike Vautour <mike@iglab.dev>
2023-06-20 00:46:20 -0400

    Broken Build

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Browser Router as Router, Switch, Route } from 'react-router-dom'
 import { useApi } from './useApi'
 import LoadingSpinner from './LoadingSpinner'
 import ErrorMessage from './ErrorMessage'
